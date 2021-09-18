32426052966b739f5445a4a5af5b546a3c0918e3
Vladimir <vi142696@gmail.com>
2021-09-18 22:32:18 +0300

    Fix bug in work_directory

--- work_directory.py
+++ work_directory.py
@@ -10,21 +10,29 @@ for dir in [work_dir, output_dir]:
     if not isdir(dir):
         print(f"Directory \"{dir}\" is not exist, creating..")
         mkdir(dir)
+
 seg_model = segmenation_model.create_model()
-seg_model.load_weights("srv/seg_model_w/weights")
+seg_model.load_weights("srv/seg_model_w/weights.h5")
 print("Model loaded")
 
 images = listdir(work_dir)
 print(f"Loaded {len(images)} images from \"{work_dir}\".")
+
 boot_time = time()
 for image in images:
-    if image.split(".")[1] not in ["jpg", "png", "jpeg", "webp", "bmp"]:
+    if image.split(".")[1] not in ["jpg", "png", "jpeg", "webp", "bmp", "gif"]:
         print(f"File {image} incorrect")
         continue
     start_time = time()
     print(f"Working {image}")
     img_orig = img_to_array(load_img(joinpath(work_dir, image)))
+    size = img_orig.shape[:2]
     img, mask = segmenation_model.predict(seg_model, img_orig)
-    segmenation_model.visualize(joinpath(output_dir, image), img_to_array(img), img_to_array(mask[0, ...]))
+    segmenation_model.visualize_with_mask(
+        joinpath(output_dir, image),
+        img_orig, 
+        img_to_array(mask[0, ...]), 
+        size
+        )
     print(f"Image {image} worked in {round(time()-start_time, 2)}s.")
 print(f"Done, {len(images)} worked successfully in {round(time()-boot_time, 2)}s.")

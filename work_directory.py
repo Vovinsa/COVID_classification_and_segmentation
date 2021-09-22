1a25b47fcea9e767ec5a058396a19de74d8dd39b
Vladimir <vi142696@gmail.com>
2021-09-22 11:04:21 +0300

    Add functions to work_directory

--- work_directory.py
+++ work_directory.py
@@ -3,6 +3,14 @@ from srv.models import segmenation_model
 from os import listdir, mkdir
 from os.path import join as joinpath, isdir
 from time import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Process big pack of images. Put images to process to folder "input" and get it from "output" folder')
+parser.add_argument('--mask', dest='make_mask', default=False,
+                    help='would put mask images into "output" directory', required=True, type=bool, action=argparse.BooleanOptionalAction)
+parser.add_argument('--overlay', dest='make_overlay', default=False,
+                    help='would put overlay of image and mask into "output" directory', required=True, type=bool, action=argparse.BooleanOptionalAction)
+args = parser.parse_args()
 
 work_dir = "input"
 output_dir = "output"
@@ -11,8 +19,10 @@ for dir in [work_dir, output_dir]:
         print(f"Directory \"{dir}\" is not exist, creating..")
         mkdir(dir)
 
+lung_seg_model = segmenation_model.create_model()
+lung_seg_model.load_weights("srv/lungs_segmentation_model_weights/weights.h5")
 seg_model = segmenation_model.create_model()
-seg_model.load_weights("srv/defeats_segmentation_model_weights/weights.h5")
+seg_model.load_weights("srv/affections_segmentation_model_weights/weights.h5")
 print("Model loaded")
 
 images = listdir(work_dir)
@@ -27,12 +37,17 @@ for image in images:
     print(f"Working {image}")
     img_orig = img_to_array(load_img(joinpath(work_dir, image)))
     size = img_orig.shape[:2]
-    img, mask = segmenation_model.predict(seg_model, img_orig)
-    segmenation_model.visualize_with_mask(
-        joinpath(output_dir, image),
-        img_orig, 
-        img_to_array(mask[0, ...]), 
-        size
-        )
+
+    lung_mask = segmenation_model.predict(lung_seg_model, img_orig)
+    mask = segmenation_model.predict(seg_model, img_orig)
+    if args.make_mask:
+        segmenation_model.correct_affections(joinpath(output_dir, "mask_"+image), lung_mask, mask, size)
+    if args.make_overlay:
+        segmenation_model.visualize_with_mask(
+            joinpath(output_dir, "overlay_"+image),
+            img_orig, 
+            img_to_array(mask[0, ...]), 
+            size
+            )
     print(f"Image {image} worked in {round(time()-start_time, 2)}s.")
 print(f"Done, {len(images)} worked successfully in {round(time()-boot_time, 2)}s.")
